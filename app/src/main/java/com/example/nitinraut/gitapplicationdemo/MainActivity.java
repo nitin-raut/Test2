f0d5da8fc625398afaf1c516f016b0e0ada23db2
nitin-raut <nitin.raut99@gmail.com>
2018-08-01 11:23:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nitinraut/gitapplicationdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.nitinraut.gitapplicationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+           }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("","");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
